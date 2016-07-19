46a0fceafd86214052f998c8b717d728211d2cf5
s29jin <s29jin@uwaterloo.ca>
2016-07-19 16:58:20 -0400

    edit on delay table view

--- Food Tracker/DelayTableViewController.swift
+++ Food Tracker/DelayTableViewController.swift
@@ -46,6 +46,9 @@ class DelayTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem()
+        
         // Load saved delayed tasks
         initTasks()
     }
@@ -76,6 +79,24 @@ class DelayTableViewController: UITableViewController {
         // Configure the cell...
         
         return cell
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tasks.removeAtIndex(indexPath.row)
+            saveTasks(tasks, url: Task.ArchiveURLDelay)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
     }
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
