c3ee6b6a0c90d7b5f205fb9b48df0b79f825af53
s29jin <s29jin@uwaterloo.ca>
2016-07-19 16:56:03 -0400

    editing on done table view

--- Food Tracker/DoneTableViewController.swift
+++ Food Tracker/DoneTableViewController.swift
@@ -36,6 +36,9 @@ class DoneTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem()
+        
         // Load done tasks
         if let savedTasks = loadTasks(Task.ArchiveURLDone) {
             tasks = savedTasks
@@ -57,6 +60,24 @@ class DoneTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return tasks.count
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tasks.removeAtIndex(indexPath.row)
+            saveTasks(tasks, url: Task.ArchiveURLDone)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
     }
 
     
