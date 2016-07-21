76062748839edd894714b4440146be826a09fa8c
Samantha Lauer <samanthalauer@v1020-wn-44-144.campus-dynamic.uwaterloo.ca>
2016-07-21 15:25:23 -0400

    persist contacts

--- Food Tracker/Contact.swift
+++ Food Tracker/Contact.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Contact {
+class Contact:NSObject{
     
     //MARK: Properties
     
@@ -16,6 +16,18 @@ class Contact {
     var nickName:String
     var email:String
     var photo:UIImage?
+    
+    //MARK: Types
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let nickNameKey = "nickName"
+        static let emailKey = "email"
+        static let photoKey = "photo"
+    }
+    
+    //MARK: Archiving Paths
+//    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+//    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("contacts")
     
     // MARK: Initialization
     init?(name: String, nickName: String, email: String, photo: UIImage?) {
@@ -25,11 +37,31 @@ class Contact {
         self.photo = photo
         self.nickName = nickName
         self.email = email
+        
+//        super.init()
     
         if name.isEmpty {
             return nil
         }
         
     }
+    
+    // MARK: NSCoding
+//    func encodeWithCoder(aCoder: NSCoder) {
+//        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+//        aCoder.encodeObject(nickName, forKey: PropertyKey.nickNameKey)
+//        aCoder.encodeObject(email, forKey: PropertyKey.emailKey)
+//        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+//    }
+//    
+//    required convenience init?(coder aDecoder:NSCoder) {
+//        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+//        let nickName = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+//        let email = aDecoder.decodeObjectForKey(PropertyKey.emailKey) as! String
+//        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as! UIImage
+//        
+//        self.init(name:name, nickName: nickName, email: email, photo: photo)
+//    }
+    
     
 }
--- Food Tracker/recipientTableViewController.swift
+++ Food Tracker/recipientTableViewController.swift
@@ -13,17 +13,34 @@ class recipientTableViewController: UITableViewController {
     //MARK: Properties
     var contacts = [Contact]()
     
-    // make a pull request to server for contact list, if no connection provide error
-    // OR
-    // get contacts from persistent data
-    
-    
-    
     //MARK: Action
     
     @IBAction func cancel(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem()
+        
+        // Load any saved meals, otherwise load sample data.
+//        if let savedContacts = loadContacts() {
+//            contacts += savedContacts
+//        }
+    }
+    
+    //MARK: NSCoding
+    /*func saveContacts() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(contacts, toFile: Contact.ArchiveURL.path!)
+        if !isSuccessfulSave {
+            print ("Failed to save meals...")
+        }
+    }*/
+    
+//    func loadContacts() -> [Contact]? {
+//        return NSKeyedUnarchiver.unarchiveObjectWithFile(Contact.ArchiveURL.path!) as? [Contact]
+//    }
 
 }
