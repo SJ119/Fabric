9080c4f58c7af7385cd48824a39ae9f7c4edbe8f
s29jin <s29jin@uwaterloo.ca>
2016-07-23 18:32:31 -0400

    partial changes need to fix nil table view values

--- Food Tracker/ContactInfoViewController.swift
+++ Food Tracker/ContactInfoViewController.swift
@@ -8,23 +8,35 @@
 
 import UIKit
 
-class ContactInfoViewController: UIViewController, UINavigationControllerDelegate {
+class ContactInfoViewController: UIViewController, UINavigationControllerDelegate,  UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     // When page is called it fills this value with the contact that was clicked on
+    
+    var tasks = [Task]()
     
     //MARK: Properties
     @IBOutlet weak var ContactPhoto: UIImageView!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var nickNameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var contact: Contact?
-    
-    // MARK: Action
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.registerClass(TaskTableViewCell.self, forCellReuseIdentifier: "taskcell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tasks += [Task(name: "a", desc: "b", dueDate: nil, status: "Current", visible: true)!]
+        self.tableView.reloadData()
+        
         // Handle the text field's user input through delegate callbacks
         //nameTextField.delegate = self
+        
+        
+        
+//        self.addGroup.delegate = self
+
         
         // Set up views if editing an existing Contact.
         if let contact = contact {
@@ -41,6 +53,34 @@ class ContactInfoViewController: UIViewController, UINavigationControllerDelegat
 
         
         //Enable the Save button only if the text field has a valid Task name.
+    }
+    
+    // MARK: Action
+    
+    @IBAction func refreshTableView(sender: AnyObject) {
+        
+        print("test")
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell : TaskTableViewCell = tableView.dequeueReusableCellWithIdentifier("taskcell", forIndexPath: indexPath) as! TaskTableViewCell
+        
+        cell.nameLabel.text = tasks[indexPath.row].name
+        let date = tasks[indexPath.row].dueDate
+        let dateFormater = NSDateFormatter()
+        dateFormater.dateStyle = .MediumStyle
+        dateFormater.timeStyle = .ShortStyle
+        
+        let datestring = dateFormater.stringFromDate(date!)
+        cell.DateLabel.text = datestring
+
+        return cell
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
